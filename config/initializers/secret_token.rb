be85a898da3f0114cf03373c7cead1e892a1580e
Steve Schofield <stevebschofield@gmail.com>
2014-02-01 12:53:16 +0000

    initial commit of new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Houndalytics::Application.config.secret_key_base = '58cd5dc434ebc213d1ca2f29e56b9f1eea572af0456bb1cfa42412bb4a8341e20a7f87c370e733d9e74a113b061537c9e9606fddbb1f3112fc5c2c673a34695c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Houndalytics::Application.config.session_store :cookie_store, key: '_houndalytics_session'
